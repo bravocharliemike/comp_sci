9573fb47249ae7ba1cf4affd55af2dfdd9f1bf7c
bravocharliemike <bernardo.camejo@hotmail.com>
2020-08-10 20:48:15 +1000

    Updated with main file

--- number_test.py
+++ number_test.py
@@ -12,4 +12,42 @@ def random_list(quantity, minimum, maximum):
     final_list = [random.randint(minimum, maximum) for number in range(quantity)]
     return final_list
 
-# TODO implement all the code from the Linux machine
+# Initialise a pool of questions to ask the user
+question_list = [
+    'Q1',
+    'Q2',
+    'Q3',
+    'Q4',
+    'Q5'
+]
+
+print('Welcome to the Number List Test program')
+while True:
+    level = input('Select a difficulty:\n'
+                  '[E]asy\n'
+                  '[M]edium\n'
+                  '[H]ard\n'
+                  '> ')
+    if level.upper() == 'E':
+        set_difficulty_level('E')
+        break
+    elif level.upper() == 'M':
+        set_difficulty_level('M')
+        break
+    elif level.upper() == 'H':
+        set_difficulty_level('H')
+        break
+    else:
+        print('Invalid choice! Enter E, M or H.')
+
+
+score = 0
+for index, question in enumerate(range(questions)):
+    print(f'Question {index + 1} of {questions}')
+    question = random.choice(question_list)
+    numbers_list = random_list(quantity, minimum, maximum)
+    print(f'!!! These are the numbers to challenge the user --> {numbers_list}')
+    # TODO Get user response and compare to right answer
+    # TODO Generate challenge question
+
+# TODO Finalise the test and show the score
