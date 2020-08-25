f1f5994f90b47ede68f030e49f041a1f4af3c92f
Bernie <u7050370@anu.edu.au>
2020-08-25 20:43:26 +1000

    Added number of tests input

--- programming_principles/module_5/test_gen.py
+++ programming_principles/module_5/test_gen.py
@@ -11,11 +11,18 @@ while True:
     else:
         print('Invalid input!')
 
+while True:
+    try:
+        num_of_tests = int(input('Enter number of tests to generate: '))
+        break
+    except ValueError:
+        print('Only enter an integer!')
+
 print('Name: _________________\n\n')
 
 # This list will keep track of questions asked by the generator
 questions = []
-for i in range(10):
+for i in range(num_of_tests):
     while True:
         num_1 = random.randint(0, 9)
         num_2 = random.randint(0, 9)
