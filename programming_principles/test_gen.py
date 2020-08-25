9c4fc41c4558858eedca9ff7ef5a8b21ee0d001b
Bernie <u7050370@anu.edu.au>
2020-08-25 18:40:29 +1000

    Finished program

--- programming_principles/test_gen.py
+++ programming_principles/test_gen.py
@@ -13,8 +13,21 @@ while True:
 
 print('Name: _________________\n\n')
 
+# This list will keep track of questions asked by the generator
+questions = []
 for i in range(10):
-    num_1 = random.randint(0, 9)
-    num_2 = random.randint(0, 9)
+    while True:
+        num_1 = random.randint(0, 9)
+        num_2 = random.randint(0, 9)
+
+        if (test_type == '-') and (num_2 > num_1):
+            num_1, num_2 = num_2, num_1
+
+        # Tuple holds the generated random numbers
+        current_question = (num_1, num_2)
+        # Check if the question has been asked already
+        if current_question not in questions:
+            break
 
     print(f'{num_1} {test_type} {num_2} = ___________\n')
+    questions.append(current_question)
